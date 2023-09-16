32f4ea08219a1b1b83574f720c388561bc6f1e16
임채리 <limch9eri@google.com>
2023-09-16 20:47:22 +0900

    feat: splash 화면 적용

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import Router from './Router';
 import { RecoilRoot } from 'recoil';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 2000);
+  }, []);
   return (
     <RecoilRoot>
       <Router />

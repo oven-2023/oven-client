5f43a42289b36690610a8e599edfa0d18f9813f1
임채리 <limch9eri@google.com>
2023-09-16 20:34:23 +0900

    fix: 채팅 홈 구조 수정

--- src/screens/Chat/ChatHomeScreen.js
+++ src/screens/Chat/ChatHomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { SafeAreaView, Text, Button, ScrollView } from 'react-native';
 import styled from 'styled-components';
 import OttList from '../../components/Main/DetailScreen/OttList';
-import OttButtonList from '../../components/Chat/OttButtonList';
+import OttButtonList from '../../components/Subscription/OttButtonList';
 import ChatRoomButton from '../../components/Chat/ChatRoomButton';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import MainLayout from '../../components/Layout/MainLayout';
@@ -11,55 +11,51 @@ const ChatHomeScreen = ({ navigation }) => {
   const rooms = [
     {
       id: 1,
-      name: '방1',
-      desc: '방1번입니다.',
+      name: '참여방1',
+      desc: '참여방1번입니다.',
       wholenum: 4,
       leftnum: 0,
     },
     {
       id: 2,
-      name: '방2',
-      desc: '방2번입니다.',
+      name: '참여방2',
+      desc: '참여방2번입니다.',
       wholenum: 4,
       leftnum: 0,
     },
     {
       id: 3,
-      name: '방3',
-      desc: '방3번입니다.',
+      name: '참여방3',
+      desc: '참여방3번입니다.',
       wholenum: 4,
       leftnum: 0,
     },
     {
       id: 4,
-      name: '방4',
-      desc: '방4번입니다.',
+      name: '참여방4',
+      desc: '참여방4번입니다.',
       wholenum: 4,
       leftnum: 0,
     },
     {
       id: 4,
-      name: '방4',
-      desc: '방4번입니다.',
+      name: '참여방4',
+      desc: '참여방4번입니다.',
       wholenum: 4,
       leftnum: 0,
     },
     {
       id: 4,
-      name: '방4',
-      desc: '방4번입니다.',
+      name: '참여방4',
+      desc: '참여방4번입니다.',
       wholenum: 4,
       leftnum: 0,
     },
   ];
   return (
     <MainLayout>
-      <OttBtnContainer>
-        <OttButtonList />
-      </OttBtnContainer>
-      <Centralizer>
         <Scroller>
-          <SubTitle>참여 중인 구독방</SubTitle>
+          <SubTitle>내가 참여 중인 구독방</SubTitle>
           <ChatRoomListContainer>
             {rooms.map(({ id, name, desc, wholenum, leftnum }) => (
               <Touchable onPress={() => navigation.navigate('ChatRoomScreen')}>
@@ -74,7 +70,6 @@ const ChatHomeScreen = ({ navigation }) => {
             ))}
           </ChatRoomListContainer>
         </Scroller>
-      </Centralizer>
     </MainLayout>
   );
 };
@@ -88,27 +83,9 @@ const Scroller = styled.ScrollView`
   width: 100%;
 `;
 
-const Centralizer = styled.View`
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const OttBtnContainer = styled.View`
-  margin: 0px 20px;
-  height: 130px;
-  flex-direction: column;
-`;
-
 const ChatRoomListContainer = styled.View`
   width: 100%;
   margin-top: 10px;
-`;
-
-const ChatButton = styled.Text`
-  width: 100px;
-  height: 100px;
-  background-color: pink;
 `;
 
 const SubTitle = styled.Text`
@@ -116,6 +93,7 @@ const SubTitle = styled.Text`
   margin-left: 20px;
   font-size: 26px;
   font-weight: 500;
+  margin-top: 8px;
 `;
 
 export default ChatHomeScreen;
--- src/screens/Subscription/SubHomeScreen.js
+++ src/screens/Subscription/SubHomeScreen.js
@@ -1,12 +1,113 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import styled from 'styled-components';
+import MainLayout from '../../components/Layout/MainLayout';
+import OttButtonList from '../../components/Subscription/OttButtonList';
+import ChatRoomButton from '../../components/Chat/ChatRoomButton';
 
 const SubscriptionScreen = () => {
+  const rooms = [
+    {
+      id: 1,
+      name: '구독방1',
+      desc: '구독방1번입니다.',
+      wholenum: 4,
+      leftnum: 0,
+    },
+    {
+      id: 2,
+      name: '구독방2',
+      desc: '구독방2번입니다.',
+      wholenum: 4,
+      leftnum: 0,
+    },
+    {
+      id: 3,
+      name: '구독방3',
+      desc: '구독방3번입니다.',
+      wholenum: 4,
+      leftnum: 0,
+    },
+    {
+      id: 4,
+      name: '구독방4',
+      desc: '구독방4번입니다.',
+      wholenum: 4,
+      leftnum: 0,
+    },
+    {
+      id: 4,
+      name: '구독방4',
+      desc: '구독방4번입니다.',
+      wholenum: 4,
+      leftnum: 0,
+    },
+    {
+      id: 4,
+      name: '구독방4',
+      desc: '구독방4번입니다.',
+      wholenum: 4,
+      leftnum: 0,
+    },
+  ];
   return (
-    <View>
-      <Text>구독</Text>
-    </View>
+    <MainLayout>
+      <OttBtnContainer>
+        <OttButtonList />
+      </OttBtnContainer>
+      <Centralizer>
+        <Scroller>
+          <SubTitle>참여 가능한 구독방</SubTitle>
+          <ChatRoomListContainer>
+            {rooms.map(({ id, name, desc, wholenum, leftnum }) => (
+              <Touchable onPress={() => navigation.navigate('ChatRoomScreen')}>
+                <ChatRoomButton
+                  id={id}
+                  name={name}
+                  desc={desc}
+                  wholenum={wholenum}
+                  leftnum={leftnum}
+                />
+              </Touchable>
+            ))}
+          </ChatRoomListContainer>
+        </Scroller>
+      </Centralizer>
+    </MainLayout>
   );
 };
+
+const Touchable = styled.TouchableOpacity`
+  width: 100%;
+  align-items: center;
+`;
+
+const Scroller = styled.ScrollView`
+  width: 100%;
+`;
+
+const Centralizer = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const OttBtnContainer = styled.View`
+  margin: 0px 20px;
+  height: 130px;
+  flex-direction: column;
+`;
+
+const ChatRoomListContainer = styled.View`
+  width: 100%;
+  margin-top: 10px;
+`;
+
+const SubTitle = styled.Text`
+  margin-right: auto;
+  margin-left: 20px;
+  font-size: 26px;
+  font-weight: 500;
+`;
 
 export default SubscriptionScreen;

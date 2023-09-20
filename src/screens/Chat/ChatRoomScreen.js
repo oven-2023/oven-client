0b4101c690530324fcbb5eac8462b65dc0900dc0
ch9eri <limch9eri@gmail.com>
2023-09-20 13:48:12 +0900

    채팅 컴포넌트 진행 중

--- src/screens/Chat/ChatRoomScreen.js
+++ src/screens/Chat/ChatRoomScreen.js
@@ -1,15 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import WebSocket from 'react-native-websocket';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  SafeAreaView,
+  Image,
+} from "react-native";
+import WebSocket from "react-native-websocket";
+import styled from "styled-components";
+import { Dimensions } from "react-native";
 
 const ChatRoomScreen = ({ navigation }) => {
   const [ws, setWs] = useState(null);
-  const [message, setMessage] = useState('');
-  const [receivedMessage, setReceivedMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const [receivedMessage, setReceivedMessage] = useState("");
 
   useEffect(() => {
     // 웹소켓 서버 주소를 설정합니다.
-    const serverUrl = 'ws://your-websocket-server-url';
+    const serverUrl = "ws://your-websocket-server-url";
     // WebSocket 인스턴스를 생성하고 서버와 연결합니다.
     const wsInstance = new WebSocket(serverUrl);
     setWs(wsInstance);
@@ -29,25 +38,56 @@ const ChatRoomScreen = ({ navigation }) => {
 
   // 메시지를 전송하는 함수
   const sendMessage = () => {
-    if (ws && message.trim() !== '') {
+    if (ws && message.trim() !== "") {
       ws.send(message);
-      setMessage('');
+      setMessage("");
     }
-    alert('전송');
+    alert("전송");
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Received Message: {receivedMessage}</Text>
-      <TextInput
-        value={message}
-        onChangeText={setMessage}
-        placeholder="Type your message..."
-        style={{ borderWidth: 1, padding: 10, width: '80%' }}
-      />
-      <Button title="Send" onPress={sendMessage} />
-    </View>
+    <SafeAreaView>
+      <ScreenContainer>
+        <MsgContainer>
+          <SingleMsg>
+            <UserImg />
+            <UserName>임채리</UserName>
+          </SingleMsg>
+        </MsgContainer>
+        <MsgInput value={message} onChangeText={setMessage} />
+      </ScreenContainer>
+    </SafeAreaView>
+
+    // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //   <Text>Received Message: {receivedMessage}</Text>
+    //   <TextInput
+    //     value={message}
+    //     onChangeText={setMessage}
+    //     placeholder="Type your message..."
+    //     style={{ borderWidth: 1, padding: 10, width: '80%' }}
+    //   />
+    //   <Button title="Send" onPress={sendMessage} />
+    // </View>
   );
 };
+
+const ScreenContainer = styled.View`
+  width: 100%;
+  align-items: center;
+`;
+
+const MsgContainer = styled.View`
+  background-color: skyblue;
+  width: 100%;
+  height: ${Dimensions.get("window").height * 0.6}px;
+`;
+
+const SingleMsg = styled.View``;
+
+const UserImg = styled.Image``;
+
+const UserName = styled.Text``;
+
+const MsgInput = styled.TextInput``;
 
 export default ChatRoomScreen;
